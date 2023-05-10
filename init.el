1334045cf523a8f73cb9bb0a075540ac5f45591d
Jesse J. Anderson <jessejanderson@gmail.com>
2023-05-10 15:40:06 -0700

    remove unnecessary `:ensure t`

--- init.el
+++ init.el
@@ -56,12 +56,10 @@
   (ivy-mode 1))
 
 (use-package doom-modeline
-  :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 22)))
 
 (use-package doom-themes
-  :ensure t
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
