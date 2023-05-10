e2aec726e121180cfe9457b0e5ef1e5a9a72fa96
Jesse J. Anderson <jessejanderson@gmail.com>
2023-05-10 13:27:41 -0700

    add auto custom-set-vairables/faces

--- init.el
+++ init.el
@@ -75,3 +75,22 @@
 (global-undo-tree-mode t)
 (add-hook 'evil-local-mode-hook 'turn-on-undo-tree-mode)
 
+;;  
+;;  
+;; Auto-set stuff below
+;;  
+;;  
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(counsel use-package undo-tree ivy evil command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
