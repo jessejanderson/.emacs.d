db4b7f88550520695872f71fb51acfee834a6d71
Jesse J. Anderson <jessejanderson@gmail.com>
2023-05-10 17:07:17 -0700

    replace evil-mode setup

--- init.el
+++ init.el
@@ -115,8 +115,42 @@
 ;;
 ;;
 
-(require 'evil)
-(evil-mode 1)
+(defun j/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+		  
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-d-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :hook (evil-mode . j/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  ;;(evil-set-initial-state 'messages-buffer-mode 'normal)
+  ;;(evil-set-initial-state 'dashboard-mode 'normal))
+)
+
+;; (require 'evil)
+;; (evil-mode 1)
 
 ;; Add easy commenting shortcut
 (evil-global-set-key 'normal (kbd "M-;") 'comment-line)
