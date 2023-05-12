f0cf3f7aaff990066cd1f750591e03904e314f83
Jesse J. Anderson <jessejanderson@gmail.com>
2023-05-11 18:16:39 -0700

    Add org mode and config

--- init.el
+++ init.el
@@ -250,6 +250,39 @@
 
 (use-package forge)
 
+(defun jj/org-mode-setup ()
+  (org-indent-mode)
+  ;; (variable-pitch-mode 1)
+  ;; (auto-fill-mode 0)
+  ;; (setq evil-auto-indent nil)
+  (visual-line-mode 1))
+
+(use-package org
+  :hook (org-mode . jj/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▼"
+	org-hide-emphasis-markers nil))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;;  Replace list hyphen with dot
+(font-lock-add-keywords 'org-mode
+			'(("^ *\\([-]\\) "
+			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(require 'org-indent)
+
+(defun jj/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . jj/org-mode-visual-fill))
+
 ;; (defun jj/evil-hook ()
 ;;   (dolist (mode '(custom-mode
 ;; 		  eshell-mode
