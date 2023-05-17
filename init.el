5223d2d953132b525abb8314d2f91b4b663bb9df
Jesse J. Anderson <jessejanderson@gmail.com>
2023-05-17 11:23:13 -0700

    comment out unused jj/evil-hook

--- init.el
+++ init.el
@@ -182,7 +182,7 @@
   ;; (setq evil-want-C-u-scroll t)
   (setq evil-want-C-d-scroll t)
   (setq evil-want-C-i-jump nil)
-  :hook (evil-mode . jj/evil-hook)
+  ;; :hook (evil-mode . jj/evil-hook)
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
